20618be0c5858a854e4fde55c1ba1a8d71737722
JuanVillagra <villagrajuan99@gmail.com>
2026-06-15 13:02:22 -0300

    Restaurar la versión base de Android

--- Assets/Scripts/ControlAcelerometro.cs
+++ Assets/Scripts/ControlAcelerometro.cs
@@ -4,81 +4,53 @@ using UnityEngine;
 
 public class ControlAcelerometro : MonoBehaviour
 {
-    public GameObject panelPausa;
-
+    // Puedes ajustar estos valores según sea necesario
     public float factorDeVelocidadX = 25.0f;
     public float factorDeVelocidadY = 25.0f;
 
-    // Velocidad de retroceso (S)
-    public float velocidadRetroceso = 0.5f;
+   
 
-    // Límites de la pista (ajustar según sea necesario)
-    private  float limiteIzquierdo = -17f;
-    private float limiteDerecho = 15f;
-
-    bool estaPausado = false;
-
-    public void ReanudarJuego()
-    {
-        Time.timeScale = 1f;
-        estaPausado = false;
-        panelPausa.SetActive(false);
-    }
-
+    // Update is called once per frame
     void Update()
     {
-        // PAUSA
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (estaPausado)
-            {
-                Time.timeScale = 1f;
-                estaPausado = false;
-                panelPausa.SetActive(false);
-            }
-            else
-            {
-                Time.timeScale = 0f;
-                estaPausado = true;
-                panelPausa.SetActive(true);
-            }
-        }
-
         Vector3 dir = Vector3.zero;
 
-#if UNITY_ANDROID
         dir.x = -Input.acceleration.x;
         dir.y = Input.acceleration.y;
-#else
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        dir.z = Input.acceleration.z;
 
-        dir.x = moveX;
-
-        // Si retrocede (S), va más lento
-        if (moveY < 0)
-        {
-            dir.y = moveY * velocidadRetroceso;
-        }
-        else
-        {
-            dir.y = moveY;
-        }
-#endif
+        //Debug.Log(dir.x.ToString());
 
         if (dir.sqrMagnitude > 1)
             dir.Normalize();
 
         dir *= Time.deltaTime;
 
+        // Ahora, mueve el objeto en ambos ejes X e Y
         transform.Translate(new Vector3(
             dir.x * factorDeVelocidadX,
-            dir.y * factorDeVelocidadY,
+            dir.y * -factorDeVelocidadY,
             0));
 
-        // Limitar movimiento dentro de la pista
-        Vector3 posicion = transform.position;
-        posicion.x = Mathf.Clamp(posicion.x, limiteIzquierdo, limiteDerecho);
-        transform.position = posicion;
+        
+    }
+
+    Vector3 GetAccelerometerValue()
+    {
+        Vector3 acc = Vector3.zero;
+        float period = 0.0f;
+
+        foreach (AccelerationEvent evt in Input.accelerationEvents)
+        {
+            acc += evt.acceleration * evt.deltaTime;
+            period += evt.deltaTime;
+        }
+
+        if (period > 0)
+        {
+            acc *= 1.0f / period;
+        }
+
+        return acc;
     }
 }
